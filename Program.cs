adff201832affc1899017e845d4c1e800bb4b017
allan02machado <70041162+allan02machado@users.noreply.github.com>
2020-11-20 20:06:20 -0300

    este commit adiciona resposta ao exercício

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace Patinhos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int duckzin;
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Five little ducks - The Wiggles");
+            Console.ResetColor();
+
+            Console.WriteLine("Digite a quantidade de patinhos deseja na música ");
+            Console.Write("(Mínimo 2): ");
+            duckzin = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine();
+
+            for(int i = duckzin; i>0; i -= 1)
+            {
+                Console.WriteLine($"{i} " + "Patinhos foram passear");
+                Console.WriteLine("Além das montanhas");
+                Console.WriteLine("Para brincar");
+                Console.WriteLine("A mamãe gritou: Quá, quá, quá, quá");
+                Console.WriteLine("Mas só " + (i - 1) + " patinhos voltaram de lá");
+                Console.WriteLine();
+            }
+            Console.WriteLine("A mamãe patinha foi procurar");
+            Console.WriteLine("Além das montanhas");
+            Console.WriteLine("Na beira do mar");
+            Console.WriteLine("A mamãe gritou: Quá, qua, qua, qua");
+            Console.WriteLine($"E os " + duckzin + " patinhos voltaram de lá");
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,12 @@
 # Patinhos
 Resposta ao exercício "Patinhos"
+
+**Enunciado**
+
+Exiba a letra da música "Cinco Patinhos" (versão brasileira gravada pela Xuxa de "Five little ducks" - The Wiggles). Seu programa deve perguntar quantos patinhos o usuário deseja (pelo menos 2).
+
+**Como executar**
+
+abra o programa e digite:
+
+dotnet run
